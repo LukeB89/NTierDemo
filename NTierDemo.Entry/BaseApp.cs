f5068469bf727ffb535c9befa93556d51a233227
Luke Byrne <lukebyrne@gmail.com>
2024-01-08 23:26:01 +0000

    Added Styling Resource Dictionaries and added them to app

--- NTierDemo.Entry/BaseApp.cs
+++ NTierDemo.Entry/BaseApp.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Security.Policy;
 using System.Windows;
+using NTierDemo.Presentation;
 
-namespace NTierDemo.Presentation;
+namespace NTierDemo.Entry;
 
 public class BaseApp : Application
 {
@@ -9,6 +11,29 @@ public class BaseApp : Application
     public static void Main()
     {
         BaseApp app = new();
+        app.Resources = LoadResourceDictionaries();
         app.Run(new MainWindow());
+    }
+
+    /// <summary>
+    /// Add all resource dictionaries to the application
+    /// </summary>
+    /// <returns></returns>
+    private static ResourceDictionary LoadResourceDictionaries()
+    {
+        // Color Resources
+        ResourceDictionary uiColorsDictionary = new();
+        uiColorsDictionary.Source = new Uri("/NTierDemo.Presentation;component/Styles/UIColors.xaml", UriKind.RelativeOrAbsolute);
+
+        // Button Resources
+        ResourceDictionary uiButtonsDictionary = new();
+        uiButtonsDictionary.Source = new Uri("/NTierDemo.Presentation;component/Styles/ButtonStyles.xaml", UriKind.RelativeOrAbsolute);
+
+        // Merge the dictionaries
+        ResourceDictionary appResources = new();
+        appResources.MergedDictionaries.Add(uiColorsDictionary);
+        appResources.MergedDictionaries.Add(uiButtonsDictionary);
+
+        return appResources;
     }
 }

61b4cf18a9142ec7f375750c77f37a7d8865517f
Luke Byrne <lukebyrne@gmail.com>
2024-01-11 23:38:01 +0000

    Added IControlData, IControlCheckable, IIconData for use with generic methods and structure data

---
+++ NTierDemo.BusinessLogic/Models/Interfaces/IControlData.cs
@@ -0,0 +1,17 @@
+﻿namespace NTierDemo.BusinessLogic;
+public interface IControlData
+{
+    public string Content { get; set; }
+    public string Style { get; set; }
+}
+
+public interface IControlCheckable
+{
+    public bool IsChecked { get; set; }
+}
+
+public interface IIconData
+{
+    public string IconStyle { get; set; }
+    public int IconName { get; set; }
+}

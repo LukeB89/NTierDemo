246a6e5489f942f1ee41dfd837ada3d930bed328
Luke Byrne <lukebyrne@gmail.com>
2024-01-11 23:39:16 +0000

    Added ControlBar Screen

---
+++ NTierDemo.BusinessLogic/Screens/ControlBar.cs
@@ -0,0 +1,19 @@
+﻿using FontAwesome.Sharp;
+using System.Windows;
+
+namespace NTierDemo.BusinessLogic;
+public abstract class ControlBarBase
+{
+    public List<object> FillButton(IconChar Icon)
+    {
+        List<object> root = new();
+
+        ButtonData button = new();
+
+        button.IconStyle = "ctrlBarButtonIcon";
+        button.IconName = (int)Icon;
+        root.Add(button);
+
+        return root;
+    }
+}
---
+++ NTierDemo.Presentation/MainWindow/ControlBar.cs
@@ -0,0 +1,41 @@
+﻿using FontAwesome.Sharp;
+using NTierDemo.BusinessLogic;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace NTierDemo.Presentation;
+
+public class ControlBar : ControlBarBase, IScreenBuilder
+{
+    public List<Active> Actives { get; } = new();
+
+    public ControlBar()
+    {
+        Active active = new();
+        active.ControlType = typeof(Button);
+        active.Name = "ctrlBarClose";
+        active.Style = "ctrlBarButton";
+        active.Tag = "C_Pallet4";
+        active.Click += WindowLogic.Handle_ControlBar_ButtonClose;
+        active.Fill += (arg) => FillButton(IconChar.Xmark);
+        Actives.Add(active);
+
+        active = new();
+        active.ControlType = typeof(Button);
+        active.Name = "ctrlBarMaximise";
+        active.Style = "ctrlBarButton";
+        active.Tag = "C_Pallet6";
+        active.Click += WindowLogic.Handle_ControlBar_ButtonMaximise;
+        active.Fill += (arg) => FillButton(IconChar.WindowMaximize);
+        Actives.Add(active);
+
+        active = new();
+        active.ControlType = typeof(Button);
+        active.Name = "ctrlBarMinimise";
+        active.Style = "ctrlBarButton";
+        active.Tag = "C_Pallet8";
+        active.Click += WindowLogic.Handle_ControlBar_ButtonMinimise;
+        active.Fill += (arg) => FillButton(IconChar.WindowMinimize);
+        Actives.Add(active);
+    }
+}

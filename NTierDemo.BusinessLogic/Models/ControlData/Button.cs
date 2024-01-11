e220d73ef06cb5a2805c442622ed339b2384acba
Luke Byrne <lukebyrne@gmail.com>
2024-01-11 23:38:25 +0000

    Added ButtonData

---
+++ NTierDemo.BusinessLogic/Models/ControlData/Button.cs
@@ -0,0 +1,8 @@
+﻿namespace NTierDemo.BusinessLogic;
+public class ButtonData : IControlData, IIconData
+{
+    public string Content { get; set; } = string.Empty;
+    public string Style {  get; set; } = string.Empty;
+    public string IconStyle {  get; set; } = string.Empty;
+    public int IconName {  get; set; }
+}

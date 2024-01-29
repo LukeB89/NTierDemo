689c00f5d1a3e696688de83163fa61ae7eb94fe1
Luke Byrne <lukebyrne@gmail.com>
2024-01-29 23:44:27 +0000

    Added GridColumnData - describes how a grid should be created

---
+++ NTierDemo.BusinessLogic/Models/ControlData/Grid.cs
@@ -0,0 +1,7 @@
+﻿namespace NTierDemo.BusinessLogic;
+public class GridColumnData
+{
+    public double ColumnWidth { get; set; } // <= 10 is proportional (1*,2*,...) > 10 is exact
+    public Enums.ContainerTypes ContainerType { get; set; }
+    public Enums.ScreenNames ScreenName { get; set; }
+}
---
+++ NTierDemo.BusinessLogic/Models/Enums/ContainerTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace NTierDemo.BusinessLogic;
+public static partial class Enums
+{
+    public enum ContainerTypes
+    {
+        Unknown = 0,
+        Canvas,
+        Grid,
+        StackPanel,
+    }
+}

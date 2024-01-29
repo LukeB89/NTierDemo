15e25494ea0acd97c1548c9749b3ca25ae4b2753
Luke Byrne <lukebyrne@gmail.com>
2024-01-29 23:47:08 +0000

    Added ToDo Screen and Data

---
+++ NTierDemo.BusinessLogic/Screens/ToDo.cs
@@ -0,0 +1,17 @@
+﻿namespace NTierDemo.BusinessLogic;
+public abstract class ToDoBase
+{
+    public List<object> FillGrid()
+    {
+        List<object> root = new();
+
+        GridColumnData gridColumnData = new()
+        {
+            ColumnWidth = 200,
+            ContainerType = Enums.ContainerTypes.StackPanel,
+            ScreenName = Enums.ScreenNames.ToDoContent
+        };
+        root.Add(gridColumnData);
+        return root;
+    }
+}
---
+++ NTierDemo.Presentation/ContentScreens/ToDo.cs
@@ -0,0 +1,21 @@
+﻿using NTierDemo.BusinessLogic;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace NTierDemo.Presentation;
+public class ToDo : ToDoBase, IScreenBuilder
+{
+    
+    public List<Active> Actives { get; } = new();
+
+    public ToDo()
+    {
+        Active active = new();
+        active.ControlType = typeof(Grid);
+        active.Width = double.NaN;
+        active.Height = double.NaN;
+        active.Fill += (arg) => FillGrid();
+        Actives.Add(active);
+    }
+
+}

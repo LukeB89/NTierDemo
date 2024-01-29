fa3c07224169c535e771e9eee33570001d0f6b49
Luke Byrne <lukebyrne@gmail.com>
2024-01-29 23:41:56 +0000

    Added temp Calculator Screen

---
+++ NTierDemo.Presentation/ContentScreens/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using NTierDemo.BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace NTierDemo.Presentation;
+public class Calculator : IScreenBuilder
+{
+    public List<Active> Actives { get; } = new();
+
+    public Calculator()
+    {
+        Active active = new();
+        active.ControlType = typeof(Button);
+        active.Content = "Calculator Button";
+        active.Width = 100;
+        active.Height = 100;
+        Actives.Add(active);
+    }
+
+}
